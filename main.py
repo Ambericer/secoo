0d09ebf8f1be31a59f4c3908c60a7d4566bb43d5
TaceyWong <xinyong.wang@qq.com>
2016-09-24 19:01:09 +0800

    -

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # encoding: utf-8
 
 import os
@@ -10,4 +9,4 @@ def hellO():
     return "Haha~~~"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8080)
+    app.run(host="0.0.0.0",port=5000)
